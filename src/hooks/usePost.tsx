e6c18357d93b561eff8ebe609a6a8ca89719fc0b
Kendrick <kendrickoppong94@gmail.com>
2024-08-15 15:38:17 +0000

    set up react query

--- src/hooks/usePost.tsx
+++ src/hooks/usePost.tsx
@@ -1,6 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export async function addReport() {
+  try {
+    const response = await axios.get(`${BASE_URL}/api/user/report`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export const usePost = () => {
-  return (
-    <div>usePost</div>
-  )
-}
+  return <div>usePost</div>;
+};
